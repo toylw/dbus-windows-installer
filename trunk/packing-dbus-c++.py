b4e349255423a2b287f0f136497771c9d610c66d
MatLinuxer2 <MatLinuxer2@e8d067f0-c3ee-7cb0-6f29-9f0a0e525987>
2011-04-30 17:30:36 +0000

    * Make two pack, one for stable version, and another for developement version ( from git )

--- trunk/packing-dbus-c++.py
+++ trunk/packing-dbus-c++.py
@@ -6,16 +6,20 @@ import os
 import shutil
 import datetime
 
-# 編譯 dbus 的 dependencies
+# build dependencies of dbus
 os.system( "emerge libxml2" );
 os.system( "emerge pthread-win32" );
 os.system( "emerge expat" );
 
-# 重新編譯 libdbus
+# rebuild libdbus
+os.system( "emerge --unmerge dbus" );
+os.system( "emerge dbus" );
+
+# rebuild libdbus from git
 os.system( "emerge --unmerge dbus-src" );
 os.system( "emerge --target=gitHEAD dbus-src" );
 
-# 重新編譯 libdbus-c++
+# rebuild libdbus-c++
 os.system( "emerge --unmerge dbus-c++-src" );
 kderoot_manifest = os.path.join ( os.getenv('KDEROOT'), "manifest" )
 os.system( "DEL " +kderoot_manifest+"\\dbus-c++-src-git-bin.mft" ) 
@@ -24,21 +28,38 @@ os.system( "DEL " +kderoot_manifest+"\\dbus-c++-src-git-lib.mft" )
 os.system( "DEL " +kderoot_manifest+"\\dbus-c++-src-git-lib.ver" ) 
 os.system( "emerge --target=gitFOLLOW dbus-c++-src" );
 
-# 將編譯後的結果集成至單一目錄下
+# gather files into a directory
 dst = ".\\dbus-c++-inst"
+dst1 = ".\\dbus-c++-inst1"
+dst2 = ".\\dbus-c++-inst2"
 os.system( "MD "+dst )
+os.system( "MD "+dst1 )
+os.system( "MD "+dst2 )
 
 kderoot_build = os.path.join ( os.getenv('KDEROOT'), "build" )
-src_expat_dir = os.path.join( kderoot_build, "win32libs-bin\\expat-2.0.1\\image" )
-src_pthread_dir = os.path.join( kderoot_build, "testing\\pthreads-win32-2.7.0\\image" )
-#src_dbus_dir = os.path.join( kderoot_build, "win32libs-bin\\dbus-1.4.1-2\\image" )
-src_dbus_dir = os.path.join( kderoot_build, "win32libs-sources\\dbus-src-1.4.1-20110111\\image-mingw4-RelWithDebInfo-gitHEAD" )
-src_dbuscxx_dir = os.path.join( kderoot_build, "win32libs-sources\\dbus-c++-src-git\\image-mingw4-RelWithDebInfo-gitFOLLOW" )
+src_expat_dir    = os.path.join( kderoot_build, "win32libs-bin\\expat-2.0.1\\image" )
+src_pthread_dir  = os.path.join( kderoot_build, "testing\\pthreads-win32-2.7.0\\image" )
+src_dbus_dir     = os.path.join( kderoot_build, "win32libs-bin\\dbus-1.4.1-2\\image" )
+src_dbus_src_dir = os.path.join( kderoot_build, "win32libs-sources\\dbus-src-1.4.6-20110302\\image-mingw4-RelWithDebInfo-gitHEAD" )
+src_dbuscxx_dir  = os.path.join( kderoot_build, "win32libs-sources\\dbus-c++-src-git\\image-mingw4-RelWithDebInfo-gitFOLLOW" )
 
-os.system( "XCOPY "+src_expat_dir + " " + dst + " /E/Y" )
-os.system( "XCOPY "+src_pthread_dir + " " + dst + " /E/Y" )
-os.system( "XCOPY "+src_dbus_dir + " " + dst + " /E/Y" )
-os.system( "XCOPY "+src_dbuscxx_dir + " " + dst + " /E/Y" )
+os.system( "XCOPY "+src_expat_dir    + " " + dst1 + " /E/Y" )
+os.system( "XCOPY "+src_pthread_dir  + " " + dst1 + " /E/Y" )
+os.system( "XCOPY "+src_dbus_src_dir + " " + dst1 + " /E/Y" )
+os.system( "XCOPY "+src_dbuscxx_dir  + " " + dst1 + " /E/Y" )
 
+os.system( "XCOPY "+src_expat_dir    + " " + dst2 + " /E/Y" )
+os.system( "XCOPY "+src_pthread_dir  + " " + dst2 + " /E/Y" )
+os.system( "XCOPY "+src_dbus_dir     + " " + dst2 + " /E/Y" )
+os.system( "XCOPY "+src_dbuscxx_dir  + " " + dst2 + " /E/Y" )
+
+
+os.system( "DEL "+ dst + " /Q/S/F" )
+os.system( "XCOPY "+ dst1 + " " + dst + " /E/Y" )
+ver_num = datetime.datetime.utcnow().strftime("%Y%m%d-%H%M-dev")
+os.system( "makensis -DVERSION_NUMBER="+ver_num+" .\dbus-c++.nsi" );
+
+os.system( "DEL "+ dst + " /Q/S/F" )
+os.system( "XCOPY "+ dst2 + " " + dst + " /E/Y" )
 ver_num = datetime.datetime.utcnow().strftime("%Y%m%d-%H%M")
 os.system( "makensis -DVERSION_NUMBER="+ver_num+" .\dbus-c++.nsi" );
