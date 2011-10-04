8146c9e8161130e6ad8771526153c07b1708f04c
MatLinuxer2 <MatLinuxer2@e8d067f0-c3ee-7cb0-6f29-9f0a0e525987>
2011-10-04 11:09:13 +0000

    * update with upstream changes.

--- packing-dbus-c++.py
+++ packing-dbus-c++.py
@@ -8,24 +8,25 @@ import datetime
 
 ver_num = datetime.datetime.utcnow().strftime("%Y%m%d-%H%M")
 expat_ver    = "2.0.1"
-pthread_ver  = "2.7.0"
-dbus_ver     = "1.4.10"
+pthread_ver  = "2.8.0"
+#dbus_ver     = "gitHEAD"
+dbus_ver     = "1.4.14"
 dbus_src_ver = "1.4.10-20110302"
 dbuscxx_ver  = "gitFOLLOW"
 dbuscxx_src_ver  = "git"
 
 # build dependencies of dbus
-os.system( "emerge libxml2" );
-os.system( "emerge pthread-win32" );
-os.system( "emerge expat" );
+os.system( "emerge --update libxml2" );
+os.system( "emerge --update pthreads-src" );
+os.system( "emerge --update expat" );
 
 # rebuild libdbus from git
-os.system( "emerge --unmerge dbus-src" );
-os.system( "emerge --offline --target=%s dbus-src" % ( dbus_ver ) );
+#os.system( "emerge --unmerge dbus-src" ); # "--offline"
+os.system( "emerge --update --target=%s dbus-src" % ( dbus_ver ) );
 
 # rebuild libdbus-c++
-os.system( "emerge --unmerge dbus-c++-src" );
-os.system( "emerge --offline --target=%s dbus-c++-src" % ( dbuscxx_ver ) );
+#os.system( "emerge --unmerge dbus-c++-src" ); # "--offline"
+os.system( "emerge --update --target=%s dbus-c++-src" % ( dbuscxx_ver ) );
 
 # gather files into a directory
 dst = ".\\dbus-c++-inst"
@@ -35,7 +36,7 @@ os.system( "MD "+dst1 )
 
 kderoot_build = os.path.join ( os.getenv('KDEROOT'), "build" )
 src_expat_dir    = os.path.join( kderoot_build, "win32libs-bin\\expat-%s\\image" % ( expat_ver ) )
-src_pthread_dir  = os.path.join( kderoot_build, "testing\\pthreads-win32-%s\\image" % ( pthread_ver ) )
+src_pthread_dir  = os.path.join( kderoot_build, "win32libs-sources\\pthreads-src-%s\\image-mingw4-RelWithDebInfo-%s" % ( pthread_ver, pthread_ver ) )
 src_dbus_src_dir = os.path.join( kderoot_build, "win32libs-sources\\dbus-src-%s\\image-mingw4-RelWithDebInfo-%s" % ( dbus_src_ver, dbus_ver ) )
 src_dbuscxx_dir  = os.path.join( kderoot_build, "win32libs-sources\\dbus-c++-src-%s\\image-mingw4-RelWithDebInfo-%s" % ( dbuscxx_src_ver, dbuscxx_ver ) )
 
